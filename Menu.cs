1892a1d0bb4f043f9b00e47126c339e836d7147b
Nayab Zafir <signorzafir@gmail.com>
2024-11-04 16:03:55 +0100

    change student Feature completed

--- Menu.cs
+++ Menu.cs
@@ -9,14 +9,12 @@ namespace DBAssignment_1
     enum MenuChoice
     {
         AddNew = 1,
-        ChangeData = 2, 
-        Delete = 3, 
-        PrintAll = 4, 
-        CloseProgram =5
+        ChangeData = 2,
+        PrintAll = 3,
+        CloseProgram = 4
     }
     internal static class Menu
     {
-
         internal static int MainMenu()
         {
 
@@ -24,9 +22,8 @@ namespace DBAssignment_1
             Console.WriteLine("" +
                 "1- Register new student in Database.\n" +
                 "2- Change data for an existing student.\n" +
-                "3- Delete a student from Database.\n" +
-                "4- Display a list of all registered students.\n" +
-                "5- Close the program.");
+                "3- Display a list of all registered students.\n" +
+                "4- Close the program.");
             Console.Write("Choose Task from above to perform: ");
             int choice = Utility.GetIntInput();
             return choice;
--- Student.cs
+++ Student.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -30,14 +31,16 @@ namespace DBAssignment_1
 
 
         }
-        internal Student AddNewStudent()
+        internal void AddNewStudent()
         {
+            var stdContext = new StudenDB();
             string fname = Utility.GetInputFirstName();
             string lname = Utility.GetInputLastName();
             string city = Utility.GetInputCity();
             string program = Utility.GetInputProgram();
-            return new Student(fname, lname, city, program);
-
+            stdContext.Add(new Student(fname, lname, city, program));
+            stdContext.SaveChanges();
+            Console.WriteLine("A new Student has been registered.");
         }
         internal void PrintAllStudents()
         {
@@ -65,6 +68,100 @@ namespace DBAssignment_1
             {
                 return "Inactive";
             }
+        }
+        internal void UpdateStudentData()
+        {
+            var stdContext = new StudenDB();
+            string studentToChange;
+            int userChoice;
+            Console.WriteLine("Enter student you want to change.");
+            studentToChange = Utility.GetInputFirstName();
+            Console.WriteLine("1- To change First Name.\n" +
+                "2- To change Last Name\n" +
+                "3- To change City.\n" +
+                "4- To change Study Program\n" +
+                "5- To Activate / Deactivate a student");
+            userChoice = Utility.GetIntInput();
+            if (stdContext.students != null)
+            {
+                var std = stdContext.students.Where(s => s.FName == studentToChange).FirstOrDefault<Student>();
+                if (std != null)
+                {
+                    switch (userChoice)
+                    {
+                        case 1:
+                            {
+                                Console.WriteLine($"Current First Name: {std.FName}");
+                                std.FName = Utility.GetInputFirstName();
+                                stdContext.SaveChanges();
+                            }
+                            break;
+                        case 2:
+                            {
+                                Console.WriteLine($"Current Last Name: {std.LName}");
+                                std.LName = Utility.GetInputLastName();
+                                stdContext.SaveChanges();
+                            }
+                            break;
+                        case 3:
+                            {
+                                Console.WriteLine($"Current City: {std.City}");
+                                std.City = Utility.GetInputCity();
+                                stdContext.SaveChanges();
+                            }
+                            break;
+                        case 4:
+                            {
+                                Console.WriteLine($"Current Study Program: {std.Program}");
+                                std.Program = Utility.GetInputProgram();
+                                stdContext.SaveChanges();
+                            }
+                            break;
+                        case 5:
+                            {
+                                string input;
+                                Console.WriteLine($"Student {std.FName} {std.LName} is {std.ToString()}");
+                                if (!std.IsActive)
+                                {
+                                    Console.Write("\nEnter \"A\" to Confirm Activation : ");
+                                }
+                                else if (std.IsActive)
+                                {
+                                    Console.Write("\nEnter \"D\" to Confirm Deactivation : ");
+                                }
+                                input = Utility.GetStringInput().ToLower();
+                                
+                                if (input == "a" && !std.IsActive)
+                                {
+                                    std.IsActive = true;
+                                }
+                                else if (input  == "d" && std.IsActive)
+                                {
+                                    std.IsActive = false;
+                                }
+                                stdContext.SaveChanges();
+                            }
+                            break;
+
+                        default:
+                            break;
+                    }
+
+                    Console.WriteLine($"\nStudent data for {std.FName} {std.LName} has been updated in Student Register.\n");
+                }
+                else
+                {
+                    Console.WriteLine("No matched record found!!!");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No matched record found!!!");
+            }
+        }
+        internal void DeativateStudent()
+        {
+
         }
     }
 }
